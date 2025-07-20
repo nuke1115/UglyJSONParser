a56a4bd5038c1633a1cf5ac188629132ac7b3c41
윤석 이 <nuclearbomb280@gmail.com>
2025-07-21 00:41:29 +0900

    fixed : added bitwise operator to NodeType enum class

--- UglyJSONParser/Include/UglyJSONParser/JSONTree/NodeTypes.hpp
+++ UglyJSONParser/Include/UglyJSONParser/JSONTree/NodeTypes.hpp
@@ -3,17 +3,43 @@
 
 namespace UglyJSONParser
 {
-    enum class NodeType
+    enum class NodeType : uint8_t
     {
-        Null = 0,
-        Object = 1,
-        Array = 2,
-        String = 3,
-        Number = 4,
-        Bool = 5,
-        Root = 6,
-        Error = -1
+        ZERO_VALUE = 0b0,
+        Null    = 0b00000001,
+        Object  = 0b00000010,
+        Array   = 0b00000100,
+        String  = 0b00001000,
+        Number  = 0b00010000,
+        Bool    = 0b00100000,
+        Root    = 0b01000000,
+        Error   = 0b10000000
     };
+
+    //underlying_type_t<NodeType> == uint8_t
+    inline constexpr NodeType operator|(NodeType left, NodeType right) noexcept
+    {
+        using T = std::underlying_type_t<NodeType>;
+        return static_cast<NodeType>(static_cast<T>(left) | static_cast<T>(right));
+    }
+
+    inline constexpr NodeType operator&(NodeType left, NodeType right) noexcept
+    {
+        using T = std::underlying_type_t<NodeType>;
+        return static_cast<NodeType>(static_cast<T>(left) & static_cast<T>(right));
+    }
+
+    inline constexpr NodeType operator^(NodeType left, NodeType right) noexcept
+    {
+        using T = std::underlying_type_t<NodeType>;
+        return static_cast<NodeType>(static_cast<T>(left) ^ static_cast<T>(right));
+    }
+
+    inline constexpr NodeType operator~(NodeType left) noexcept
+    {
+        using T = std::underlying_type_t<NodeType>;
+        return static_cast<NodeType>(~static_cast<T>(left));
+    }
 }
 
 #endif // !UGLY_JSON_PARSER_NODE_TYPES_HEADER
