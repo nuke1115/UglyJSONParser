fa70ebd8f649c57a88f8e165c2195acceea892b9
윤석 이 <nuclearbomb280@gmail.com>
2025-07-21 19:37:11 +0900

    feature : added MakeErrorBitmask function

---
+++ UglyJSONParser/Include/UglyJSONParser/Utils/ResultUtils.hpp
@@ -0,0 +1,22 @@
+#ifndef UGLY_JSON_PARSER_RESULT_UTILS_HEADER
+#define UGLY_JSON_PARSER_RESULT_UTILS_HEADER
+
+#include <stdint.h>
+#include "../EnumerationInclude.hpp"
+
+namespace UglyJSONParser
+{
+    namespace ResultUtils
+    {
+        constexpr inline uint32_t MakeErrorBitmask(NodeType nodeType, DataTypes dataType, AccessTypes accessType, OperationTypes operationType, DetailedErrInfo detailedErrInfo)
+        {
+            return  static_cast<uint32_t>(nodeType)         |
+                    static_cast<uint32_t>(dataType)         |
+                    static_cast<uint32_t>(accessType)       |
+                    static_cast<uint32_t>(operationType)    |
+                    static_cast<uint32_t>(detailedErrInfo)  ;
+        }
+    }
+}
+
+#endif // !UGLY_JSON_PARSER_RESULT_UTILS_HEADER
